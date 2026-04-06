ccaff3d5457026ed6b4b18c4b79c4175dd848c0e
cherrow <cherrowvideosuggestions@gmail.com>
2026-04-06 13:56:36 -0700

    initial commit

---
+++ src/main/java/cherrow/unstableciv/UnstablecivClient.java
@@ -0,0 +1,10 @@
+package cherrow.unstableciv;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnstablecivClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
